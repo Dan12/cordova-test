7b9d8c5c1dbe2ac8329e9df75d7e89e140ec4113
Dan W <dantech88@yahoo.com>
2015-12-05 06:55:12 +0000

    new styles

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
    $("#a_button").click(function(){
       $("#hidden_text").toggle(300); 
    });
+   if(debug){
+      setTimeout(function(){
+         $(".listening").css("display","none");
+         $(".received").css("display","inline-block");
+      },2000);
+   }
 });
+
+debug = true;
