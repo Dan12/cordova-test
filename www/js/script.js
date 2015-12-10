d111b270cdcddb3ff04832073a37c9e6619c3a24
Dan W <dantech88@yahoo.com>
2015-12-10 18:42:14 +0000

    blah

--- www/js/script.js
+++ www/js/script.js
@@ -10,10 +10,76 @@ $(document).ready(function(){
    }
    
    var canvas = document.getElementById("myCanvas");
-   var ctx = canvas.getContext("2d");
+   ctx = canvas.getContext("2d");
    
-   ctx.fillStyle = "red";
-   ctx.fillRect(10,10,30,30);
+   redraw();
+   
+   setInterval(function(){
+      redraw();
+   },20);
+   
+   $("#myCanvas").click(function(){
+      r++;
+      if(r>4)
+         r = 1;
+      if(r == 1){
+         xvel = 1;
+         yvel = 0;
+      }
+      
+      if(r == 2){
+         xvel = 0;
+         yvel = 1;
+      }
+      
+      if(r == 3){
+         xvel = -1;
+         yvel = 0;
+      }
+      
+      if(r == 4){
+         xvel = 0;
+         yvel = -1;
+      }
+   });
 });
 
+function redraw(){
+   ctx.clearRect(0,0,200,200);
+   x+=xvel;
+   y+=yvel;
+   if(x < 0){
+      r = 1;
+      x = 0;
+      xvel = 1;
+      yvel = 0;
+   }
+   if(x > 200-30){
+      r = 3;
+      x = 200-30;
+      xvel = -1;
+      yvel = 0;
+   }
+   if(y < 0){
+      r = 2;
+      y = 0;
+      xvel = 0;
+      yvel = 1;
+   }
+   if(y > 200-30){
+      r = 4;
+      y = 200-30;
+      xvel = 0;
+      yvel = -1;
+   }
+   ctx.fillStyle = "red";
+   ctx.fillRect(x,y,30,30);  
+}
+
+x = 10;
+y = 10;
+xvel = 1;
+yvel = 0;
+r = 1;
 debug = false;
+ctx = null;
